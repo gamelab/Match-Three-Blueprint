3069307ed893e38e67cb80518a7346860891b896
Thyago Santos <thyagomatheus15@gmail.com>
2023-03-16 20:36:19 -0300

    Add files via upload

---
+++ src/game.js
@@ -0,0 +1,23 @@
+
+/**
+* The core EndlessRunner blueprint game file.
+* 
+* This file is only used to initalise (start-up) the main Kiwi Game 
+* and add all of the relevant states to that Game.
+*
+*/
+
+//Initialise the Kiwi Game. 
+var gameOptions = {
+	width: 520,
+	height: 600
+}
+
+var game = new Kiwi.Game('content', 'Match3', null, gameOptions);
+
+//Add all the States we are going to use.
+game.states.addState(LoadingState);
+game.states.addState(IntroState);
+game.states.addState(PlayState);
+
+game.states.switchState("LoadingState");
